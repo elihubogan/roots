1aa4330f28e86365af1e1ab77cf375a2057c525a
Elihu MacBookPro <elihubogan@gmail.com>
2015-02-02 03:18:18 -0800

    center text

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,10 +1,10 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h1 class="entry-title text-center"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
-    <div class="entry-content">
+    <div class="entry-content text-center">
       <?php the_content(); ?>
     </div>
     <footer>
